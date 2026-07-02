8b51df270769e9b270a3b585c6fa30842b30d1d2
Amune Israel <amuneisrael224@gmail.com>
2026-07-02 09:57:48 +0100

    feat(api): wire AuthModule with passport session

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Branch } from "../branches/branch.entity";
+import { Tenant } from "../tenants/tenant.entity";
+import { User } from "../users/user.entity";
+import { AuthController } from "./controllers/auth.controller";
+import { AuthService } from "./services/auth.service";
+import { SessionSerializer } from "./serializers/session.serializer";
+import { LocalStrategy } from "./strategies/local.strategy";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Tenant, Branch]),
+    PassportModule.register({ session: true }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
+  exports: [AuthService],
+})
+export class AuthModule {}
